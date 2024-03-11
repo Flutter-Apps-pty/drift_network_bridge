60f882c06a6393b41d4fe11f0dceacf636a45b72
Heinrich <ha.vannieuwenhuizen@gmail.com>
2024-03-11 16:33:14 +1000

    Update drift_network_bridge.dart

--- lib/drift_network_bridge.dart
+++ lib/drift_network_bridge.dart
@@ -8,3 +8,4 @@ export 'implementation/mqtt_database_gateway.dart';
 export 'implementation/mqtt_stream_channel.dart';
 export 'src/network_stream_channel/database_gateway.dart';
 export 'src/network_stream_channel/network_stream_channel.dart';
+export 'src/network.dart';
