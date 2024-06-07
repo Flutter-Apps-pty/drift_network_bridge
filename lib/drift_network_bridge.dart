9457522277802432e2f473043a03f2d3ff84d68c
Heinrich <ha.vannieuwenhuizen@gmail.com>
2024-06-07 14:13:02 +1000

    Update drift_network_bridge.dart

--- lib/drift_network_bridge.dart
+++ lib/drift_network_bridge.dart
@@ -2,6 +2,7 @@
 library drift.network_bridge;
 
 export 'src/network.dart';
+export 'src/bridge/interfaces/base/drift_bridge_interface.dart';
 export 'src/bridge/interfaces/drift_mqtt_interface.dart';
 export 'src/bridge/interfaces/drift_tcp_interface.dart';
 export 'src/drift_bridge_server.dart';
